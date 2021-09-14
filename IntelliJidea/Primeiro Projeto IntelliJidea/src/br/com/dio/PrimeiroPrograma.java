9175647655756826764679c080324f0e4b6f69c0
saulobt <saulobt@gmail.com>
2021-09-14 16:14:53 -0300

    Criando Class Livro

--- IntelliJidea/Primeiro Projeto IntelliJidea/src/br/com/dio/PrimeiroPrograma.java
+++ IntelliJidea/Primeiro Projeto IntelliJidea/src/br/com/dio/PrimeiroPrograma.java
@@ -13,7 +13,56 @@ public class PrimeiroPrograma {
         gato.setNome("Tom Cat");
         gato.setCor("Preto/Branco");
         gato.setIdade(3);
-        System.out.printf("Gato: " + gato);
+        System.out.println("Gato: " + gato);
+        Livro livro = new Livro("Arquitetura Limpa","Robert C. Martin",400);
+        System.out.println("Livro: "+livro);;
     }
 
+
+
+}
+
+ class Livro {
+    private String nomeLivro;
+    private String autor;
+    private int numeroPaginas;
+
+    public Livro(String nomeLivro, String autor, int numeroPaginas) {
+        this.nomeLivro = nomeLivro;
+        this.autor = autor;
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    public String getNomeLivro() {
+        return nomeLivro;
+    }
+
+    public void setNomeLivro(String nomeLivro) {
+        this.nomeLivro = nomeLivro;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getNumeroPaginas() {
+        return numeroPaginas;
+    }
+
+    public void setNumeroPaginas(int numeroPaginas) {
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nomeLivro=" + nomeLivro +
+                ", autor=" + autor +
+                ", numeroPaginas=" + numeroPaginas +
+                '}';
+    }
 }
